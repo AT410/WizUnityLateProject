dd5301b02dd6dea9e069835a529b6b62d876fa7f
kusamura <kusa.mr_72@icloud.com>
2020-07-09 16:19:14 +0900

    UIManagerの追加、UIの動き確認用にシーン追加

---
+++ WizUnityLateProject/Assets/Scripts/CommandUI.cs
@@ -0,0 +1,148 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class CommandUI : MonoBehaviour
+{
+    // 一つのフラグですぐにメニューが立ち上がるのは避けたいが書き方的にどうなのか
+
+    // UIマネージャーの取得
+    public UIManager m_uiManager;
+    // 攻撃終了フラグ
+    [SerializeField]
+    private bool m_bAttackEnd = false;
+    // 移動終了フラグ
+    [SerializeField]
+    private bool m_bMoveEnd = false;
+    // 待機フラグ
+    [SerializeField]
+    private bool m_bWait = false;
+    // 移動中フラグ
+    [SerializeField]
+    private bool m_bMove = false;
+
+    // 1つ前の移動フラグ
+    bool m_bBeforeMove = false;
+
+    [SerializeField]
+    bool m_bLateTimeStart = false;// A押したときのタイミングをボタンUIクリックとずらす用
+    float m_lateTime = 0.1f;
+    float m_clickTime = 0.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetLateTime();
+        // Aを押した瞬間
+        if(Input.GetButtonDown("Fire1") && !m_bLateTimeStart)
+        {
+            // 待機ならもうメニューが出ないように
+            if (!m_bWait)
+            {
+                // メニューが何もアクティブになっていないかつ行動していなかったら
+                if (!m_uiManager.GetCurrentMenu() && !m_bMove)
+                {
+                    //メインメニューを開く
+                    m_uiManager.ActiveMenu(true, UIManager.MenuSelect.MainMenu);
+                }
+                //移動中なら
+                else if (m_bMove && m_bBeforeMove)
+                {
+                    //移動の終了
+                    m_bMove = false;
+                }
+            }
+        }
+        // Bを押した瞬間
+        if (Input.GetButtonDown("Fire2"))
+        {
+            // 現在アクティブになっているメニューの非表示
+            m_uiManager.ActiveMenu(false, m_uiManager.GetCurrentMenuNum());
+        }
+        // 現在のフレームで移動が始まったら
+        if(!m_bBeforeMove && m_bMove)
+        {
+            Debug.Log("移動始め");
+        }
+        // 現在のフレームで移動が終わったら
+        else if(m_bBeforeMove && !m_bMove)
+        {
+            Debug.Log("移動終わり");
+            m_bMoveEnd = true;
+        }
+        m_bBeforeMove = m_bMove;
+    }
+
+    // A押したときのタイミングをボタンUIクリックとずらす用
+    void GetLateTime()
+    {
+        if(m_bLateTimeStart)
+        {
+            m_clickTime += Time.deltaTime;
+            if(m_clickTime >= m_lateTime)
+            {
+                m_clickTime = 0.0f;
+                m_bLateTimeStart = false;
+            }
+        }
+    }
+
+    // 攻撃ボタンを押したとき
+    public void OnAttackButtonClick()
+    {
+        if (!m_bAttackEnd)
+        {
+            // 攻撃メニューを開く
+            m_uiManager.ActiveMenu(true, UIManager.MenuSelect.AttackMenu);
+        }
+    }
+
+    // 移動ボタンを押したとき
+    public void OnMoveButtonClick()
+    {
+        if (!m_bMoveEnd)
+        {
+            // 選べないボタンのカラー変更
+            m_uiManager.SetEndButtonColor("MoveButton");
+            //全メニューを非アクティブにする
+            m_uiManager.NotActiveAllMenu();
+
+            // 移動開始
+            m_bMove = true;
+            m_bLateTimeStart = true;
+        }
+    }
+
+    // 待機ボタンを押したとき
+    public void OnWaitButtonClick()
+    {
+        if(!m_bWait)
+        {
+            // 選べないボタンのカラー変更
+            m_uiManager.SetEndButtonColor("MoveButton");
+            //全メニューを非アクティブにする
+            m_uiManager.NotActiveAllMenu();
+
+            m_bLateTimeStart = true;
+            // 待機に
+            m_bWait = true;
+            Debug.Log("待機しました");
+        }
+    }
+
+    //攻撃メニューのbuttonボタンを押したとき
+    public void OnButtonInAttackClick()
+    {
+        // 選べないボタンのカラー変更
+        m_uiManager.SetEndButtonColor("AttackButton");
+        //全メニューを非アクティブにする
+        m_uiManager.NotActiveAllMenu();
+
+        // 攻撃終了
+        m_bAttackEnd = true;
+        m_bLateTimeStart = true;
+        Debug.Log("攻撃終了");
+    }
+}
---
+++ WizUnityLateProject/Assets/Scripts/UIManager.cs
@@ -0,0 +1,190 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class UIManager : MonoBehaviour
+{
+    // 今の状態だとメニューの種類 = 階層になってるかも
+
+    // メニューの種類
+    public enum MenuSelect { None, MainMenu, AttackMenu};
+
+    // 親キャンバス
+    [SerializeField]
+    private Canvas m_ParentCanvas;
+    // メインメニュー(1つ目)
+    [SerializeField]
+    private GameObject m_mainMenuPanel;
+    // 攻撃用メニュー
+    [SerializeField]
+    private GameObject m_attackMenuPanel;
+    // 現在アクティブのメニュー
+    [SerializeField]
+    private GameObject m_currentMenu;
+    // ひとつ前アクティブのメニュー
+    [SerializeField]
+    private GameObject m_beforeMenu;
+    // 現在のメニューの種類
+    [SerializeField]
+    private MenuSelect m_currentMenuNum = MenuSelect.None;
+
+    private void Awake()
+    {
+        // 1番最初にメニューは隠しておく
+        NotActiveAllMenu();
+    }
+
+    // メニューパネルの表示・非表示
+    public void ActiveMenu(bool isActive, MenuSelect menu)
+    {
+        // 初めに選択されるよう設定されていたオブジェクトを初期化
+        EventSystem.current.SetSelectedGameObject(null);
+        m_beforeMenu = m_currentMenu;
+        if (isActive)
+        {
+            // 表示
+            // 現在アクティブのメニューを更新
+            m_currentMenu = GetMenu(menu);
+            m_currentMenuNum = menu;
+            // メニューを表示
+            GetMenu(menu).SetActive(true);
+            // 操作できるボタンの設定
+            ActiveMenuButton(true, menu);
+            // 初めに選択されるオブジェクトの設定
+            EventSystem.current.SetSelectedGameObject(GetMenu(menu).transform.GetChild(0).gameObject);
+        }
+        else
+        {
+        // 非表示
+            // もともと何も入っていないなら終了
+            if (!m_currentMenu) return;
+            // メインメニュー（一つ目のメニュー）ならアクティブになっているメニューは無しに
+            if (m_currentMenu == m_mainMenuPanel)
+            {
+                m_currentMenu = null;
+            }
+            else
+            {
+                // それ以外は自分のひとつ前のメニューをアクティブに
+                m_currentMenu = GetMenu(menu - 1);
+                // 操作ボタンと初期選択オブジェクトの設定
+                ActiveMenuButton(false, menu);
+                EventSystem.current.SetSelectedGameObject(GetMenu(menu - 1).transform.GetChild(0).gameObject);
+            }
+            // ひとつ前のメニューの種類
+            m_currentMenuNum = menu - 1;
+            // メニューの非表示
+            GetMenu(menu).SetActive(false);
+        }
+    }
+
+    // メニューパネルの取得
+    public GameObject GetMenu(MenuSelect menu)
+    {
+        // メニューの種類別に取得
+        switch(menu)
+        {
+            case MenuSelect.MainMenu:// メインメニュー
+                return m_mainMenuPanel;
+            case MenuSelect.AttackMenu:// 攻撃メニュー
+                return m_attackMenuPanel;
+            default:
+                return null; ;//何もないならnull
+        }
+    }
+
+    // メニューのボタン操作のアクティブ化
+    private void ActiveMenuButton(bool isActive, MenuSelect menu)
+    {
+        // 見えているボタンオブジェクトの非アクティブ化
+        foreach (var obj in FindObjectsOfType<Button>())
+        {
+            obj.interactable = false;
+        }
+        // アクティブ化がtrueなら
+        if(isActive)
+        {
+            // 指定したメニューのアクティブ化
+            foreach(Transform obj in GetMenu(menu).transform)
+            {
+                Button button = obj.GetComponent<Button>();
+                if(button)
+                {
+                    button.interactable = isActive;
+                }
+            }
+        }
+        else
+        {
+            // アクティブ化がfalseなら最初のメニュー以外はひとつ前のボタンのアクティブ化
+
+            if (menu == MenuSelect.MainMenu) return;
+            foreach (Transform obj in GetMenu(menu - 1).transform)
+            {
+                Button button = obj.GetComponent<Button>();
+                if (button)
+                {
+                    button.interactable = true;
+                }
+            }
+        }
+    }
+
+    // 全メニュー非表示
+    public void NotActiveAllMenu()
+    {
+        // メニューごとに非表示
+        foreach (Transform child in m_ParentCanvas.transform)
+        {
+            child.gameObject.SetActive(false);
+        }
+        //現在アクティブ化メニューをnullに
+        m_currentMenu = null;
+        m_currentMenuNum = MenuSelect.None;
+    }
+
+    // ゲッター
+    // 現在アクティブのメニュー
+    public GameObject GetCurrentMenu()
+    {
+        return m_currentMenu;
+    }
+    // ひとつ前アクティブのメニュー
+    public GameObject GetBeforeMenu()
+    {
+        return m_beforeMenu;
+    }
+    // ひとつ前アクティブのメニューセット
+    public void SetBeforeMenu(GameObject menu)
+    {
+        m_beforeMenu = menu;
+    }
+    // 現在のメニューの種類
+    public MenuSelect GetCurrentMenuNum()
+    {
+        return m_currentMenuNum;
+    }
+
+    // どうにか改良したいが仮でボタンの名前取ってきて色変更
+    public void SetEndButtonColor(string buttonName)
+    {
+        foreach(var bt in FindObjectsOfType<Button>())
+        {
+            if(bt.gameObject.name == buttonName)
+            {
+                bt.gameObject.GetComponent<Image>().color = new Color(1, 0.2f, 0.2f, 1);
+            }
+        }
+
+    }
+    // 色のリセット
+    public void ResetAllButtonColor()
+    {
+        foreach (var bt in FindObjectsOfType<Button>())
+        {
+            bt.gameObject.GetComponent<Image>().color = new Color(1, 1, 1, 1);
+        }
+    }
+}
