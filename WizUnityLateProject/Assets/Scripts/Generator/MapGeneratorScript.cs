01e380f86940439914a345c713830230a7f588c2
TatsuyaOkabe <okabetatsuyaof@gmail.com>
2020-07-10 14:38:49 +0900

    [new][script]　GameStageManagerScript　MapCursorScriptを追加

---
+++ WizUnityLateProject/Assets/Scripts/Generator/MapGeneratorScript.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Xml.Linq;
+
+public class MapGeneratorScript : MonoBehaviour
+{
+    //
+    [SerializeField] List<string> m_textureName;
+    //生成させるオブジェクトの保存
+    [SerializeField] List<GameObject> m_mapObj;
+    //
+    [SerializeField] List<uint> m_mapStateNum;
+
+    //マップの半径
+    [SerializeField] uint m_cellSizeHalf;
+
+    //生成を始めるポジション
+    [SerializeField] Vector2 m_firstGeneratPos;
+    public List<Vector2> m_mapPosData;
+
+    void Awake()
+    {
+        MapGenerat();
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        LoadXML();
+    }
+
+    void LoadXML()
+    {
+
+    }
+
+    void MapGenerat()
+    {
+        int x = (int)Mathf.Sqrt(25.0f);
+        int y = (int)Mathf.Sqrt(25.0f);
+
+        for (int i = 0; i < y; i++)
+        {
+            for (int j = 0; j < x; j++)
+            {
+                //ポジジョンの設定
+                Vector3 setPos = new Vector3(m_firstGeneratPos.x + (m_cellSizeHalf * j), m_firstGeneratPos.y - (m_cellSizeHalf * i), 0.0f);
+                //回転情報の設定
+                Quaternion setQuat = new Quaternion(0.0f, 0.0f, 0.0f, 0.0f);
+                //オブジェクト生成
+                Instantiate(m_mapObj[(int)m_mapStateNum[i + j]], setPos, setQuat);
+                m_mapPosData.Add(new Vector2(setPos.x, setPos.y));
+            }
+        }
+    }
+}
---
+++ WizUnityLateProject/Assets/Scripts/MapCursorScript.cs
@@ -0,0 +1,134 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MapCursorScript : MonoBehaviour
+{
+    [Header("オブジェクト")]
+    [SerializeField] GameObject m_mapDataManagerObj;
+    [SerializeField] GameObject m_cursorObj;
+    [SerializeField] Image m_menuCursorObj;
+
+    [Header("スクリプト")]
+    [SerializeField] GameStageManagerScript m_sp_gameStageManager;
+
+    [Header("マップデータ")]
+    [SerializeField] uint m_maxMapNum;
+    [SerializeField] uint m_moveMapCursorNum;
+    [SerializeField] uint m_moveCommandCursorNum;
+    [SerializeField] List<Vector2> m_mapPosData;
+
+    [Header("クールタイム")]
+    [SerializeField] float m_inputTimer;
+    [SerializeField] float m_maxInputTimer;
+    bool m_bCoolTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_maxMapNum = (uint)m_mapDataManagerObj.GetComponent<MapGeneratorScript>().m_mapPosData.Count;
+        m_moveMapCursorNum = m_maxMapNum / 2;
+        m_mapPosData = m_mapDataManagerObj.GetComponent<MapGeneratorScript>().m_mapPosData;
+        m_cursorObj.transform.position = new Vector3(m_mapPosData[(int)m_moveMapCursorNum].x, m_mapPosData[(int)m_moveMapCursorNum]. y, 0.0f);
+
+        m_sp_gameStageManager = GameObject.Find("GameStageManager").GetComponent<GameStageManagerScript>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        InputHunder();
+        MoveCoolTime();
+    }
+
+    //
+    void InputHunder()
+    {
+        float inputHorizon = Input.GetAxis("Horizontal");
+        float inputVert = Input.GetAxis("Vertical");
+        bool bInput = inputHorizon == 0 && inputVert == 0;
+        uint cursorNum = m_moveMapCursorNum;
+        uint mapIdRad = (uint)Mathf.Sqrt(m_maxMapNum);
+
+        PushA();
+
+        bool bVisibleCommandUI = m_sp_gameStageManager.m_bVisibleCommandMenuUI;
+
+        if (!m_bCoolTime && !bVisibleCommandUI)
+        {
+            if (inputHorizon != 0)
+            {
+                cursorNum += (uint)inputHorizon;
+                MoveMapCursor(cursorNum);
+            }
+
+            if (inputVert != 0)
+            {
+                cursorNum -= (uint)inputVert * mapIdRad;
+                MoveMapCursor(cursorNum);
+            }
+        }
+
+        if (bInput)
+        {
+            m_bCoolTime = false;
+            m_inputTimer = 0;
+        }
+    }
+
+    //カーソル移動のクールタイム処理
+    void MoveCoolTime()
+    {
+        if (m_bCoolTime && m_inputTimer <= m_maxInputTimer)
+        {
+            m_inputTimer += Time.deltaTime;
+        }
+        else
+        {
+            m_bCoolTime = false;
+        }
+    }
+
+    //カーソルを動かす処理
+    void MoveMapCursor(uint cursorNum)
+    {
+        if (m_mapPosData.Count > cursorNum && 0 <= cursorNum)
+        {
+            m_moveMapCursorNum = cursorNum;
+            m_cursorObj.transform.position = new Vector3(m_mapPosData[(int)m_moveMapCursorNum].x, m_mapPosData[(int)m_moveMapCursorNum].y, 0.0f);
+        }
+        else
+        {
+            //SEを鳴らす
+        }
+        m_bCoolTime = true;
+        m_inputTimer = 0;
+    }
+
+    //Aボタンを押したとき
+    void PushA()
+    {
+        if (Input.GetKeyDown("joystick button 0"))
+        {
+            if (m_sp_gameStageManager.MenuStateNum == (int)DataBase.EN_MenuState.ChoosePlayr)
+            {
+                Debug.Log("データ転送_ChoosePlayr");
+                m_sp_gameStageManager.ChoiceMapID = m_moveMapCursorNum;
+                m_sp_gameStageManager.ConfirmationCharacter();
+            }
+
+            if (m_sp_gameStageManager.MenuStateNum == (int)DataBase.EN_MenuState.ChooseEnemy)
+            {
+                Debug.Log("データ転送_ChooseEnemy");
+
+            }
+
+            if (m_sp_gameStageManager.MenuStateNum == (int)DataBase.EN_MenuState.ChooseMap)
+            {
+                Debug.Log("データ転送_ChooseMap");
+
+            }
+        }
+    }
+}
